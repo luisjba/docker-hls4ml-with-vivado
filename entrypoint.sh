9a2b0686a013c61099ae90ce16c18943169e6a60
Jose Luis Bracamonte Amavizca <luisjba@gmail.com>
2022-05-08 02:54:47 -0700

    Entry point to support VNC option and CMD substitution by entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # VIVADO_ML_HOME_DIR => Env varibale fo Vivamo Ml installation directory.
 # VIVADO_ML_VERSION => Env variable with the target Vivao ML Version.
 
-VIVADO_EXEC=${VIVADO_ML_HOME_DIR}/Vivado/${VIVADO_ML_VERSION}/bin/vivado
 # Verify Vivao ML installation is present
+VIVADO_EXEC=${VIVADO_ML_HOME_DIR}/Vivado/${VIVADO_ML_VERSION}/bin/vivado
 if [ ! -f ${VIVADO_EXEC} ]; then
   >&2 echo "Vivia ML instalation not found in $VIVADO_EXEC"
   exit 1
@@ -13,6 +13,7 @@ fi
 if [ ! -f /home/vivado/.bashrc ]; then 
   echo "source ${VIVADO_ML_HOME_DIR}/Vivado/${VIVADO_ML_VERSION}/settings64.sh" >> /home/vivado/.bashrc
 fi 
-
-vncserver -geometry ${GEOMETRY} ${DISPLAY}
-${VIVADO_EXEC}
+if [ "$START_VNC_SERVER" = "ON" ]; then
+  vncserver -geometry ${GEOMETRY} ${DISPLAY}
+fi
+$(eval "echo $@")
